9289347a8de2f7febf25c9147210641514939023
tnrud99 <jess0925@snu.ac.kr>
2025-05-21 13:29:08 +0900

    studyarray2

--- Assets/02.Scripts/StudyArray.cs
+++ Assets/02.Scripts/StudyArray.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StudyArray : MonoBehaviour
 {
-    int[] arrayNumber = new int[5]; // 배열 선언
+    
+   List<int> listNumber = new List<int>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log($"Array의 첫번째 값 : {arrayNumber[0]}");
+       listNumber.Add(1);
+        listNumber.Add(2);
+        listNumber.Add(3);
+        listNumber.Add(4);
+        listNumber.Add(5);
+        
+        Debug.Log($"현재 List에 있는 데이터 수 : {listNumber.Count}");
+                 
     }
 
 }
