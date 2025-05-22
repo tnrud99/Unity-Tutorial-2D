cd68c51e4c87e123c1822647d791faff8595abb0
tnrud99 <jess0925@snu.ac.kr>
2025-05-22 13:27:09 +0900

    0522-01

--- Assets/02.Scripts/StudyComponent.cs
+++ Assets/02.Scripts/StudyComponent.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class StudyComponent : MonoBehaviour
 {
-    public GameObject Obj;
+    public GameObject obj;
 
-    public string changeName;
-    
     void Start()
     {
-        Obj = GameObject.Find("Main Camera");
+        // Player라는 Tag를 가진 게임오브젝트를 찾아서 obj에 할당
+        obj = GameObject.FindGameObjectWithTag("Player");
 
-        Obj.name = changeName;
+        Debug.Log(obj.name); // 게임오브젝트의 이름
+        Debug.Log(obj.tag); // 게임오브젝트의 태그
+        Debug.Log(obj.transform.position); // 게임오브젝트의 Transform 컴포넌트의 위치
+        Debug.Log(obj.transform.rotation); // 게임오브젝트의 Transform 컴포넌트의 회전
+        Debug.Log(obj.transform.localScale); // 게임오브젝트의 Transform 컴포넌트의 크기
+
+        // MeshFilter 컴포넌트에 접근해서 mesh를 Log 메세지로 출력하는 기능
+        Debug.Log($"Mesh 데이터 : {obj.GetComponent<MeshFilter>().mesh}");
+        
+        // MeshRenderer 컴포넌트에 접근해서 material을 Log 메세지로 출력하는 기능
+        Debug.Log($"Material 데이터 : {obj.GetComponent<MeshRenderer>().material}");
         
     }
     
